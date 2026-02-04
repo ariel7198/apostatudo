084f3b8abeb16d0570a8a4ad27047acf0cf3453c
ariel <ariel@geeklys.com>
2026-02-03 23:56:10 -0300

    create skeleton base

---
+++ projeto/frontend/components/SkeletonBase.vue
@@ -0,0 +1,12 @@
+<template>
+  <div
+    class="bg-gray-200 dark:bg-gray-700 rounded animate-pulse"
+    :class="className"
+  />
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  className?: string
+}>()
+</script>
