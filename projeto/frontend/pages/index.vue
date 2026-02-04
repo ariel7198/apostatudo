29b4234ea8933d75df901abf6aea84ff22573a53
ariel <ariel@geeklys.com>
2026-02-03 23:56:39 -0300

    create redirect

--- projeto/frontend/pages/index.vue
+++ projeto/frontend/pages/index.vue
@@ -1,5 +1,7 @@
-<template>
-  <div class="bg-emerald-500 text-white p-6 text-xl">
-    index
-  </div>
-</template>
+<script setup lang="ts">
+definePageMeta({
+  middleware: () => {
+    return navigateTo("/profissionais");
+  },
+});
+</script>
